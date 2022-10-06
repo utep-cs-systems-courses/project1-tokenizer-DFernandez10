90a4e33604855b57f01a7f7190b538138ada9a4b
Denisse Fernandez <dafernandez10@miners.utep.edu>
2022-10-06 12:18:41 -0600

    uploaded for coaching session / incomplete

--- src/history.c
+++ src/history.c
@@ -1,35 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "history.h"
-
-typedef struct s_Item {
-  int id;
-  char *str;
-  struct s_Item *next;
-} Item;
-
-typedef struct s_List {
-  struct s_Item *root;
-} List;
+#include "tokenizer.h"
 
 /* Initialize the linked list to keep the history. */
-List* init_history();
+List* init_history(){
+  List *pointHead = malloc(sizeof(List));
+  pointHead -> root = NULL;
+  return pointHead;
+}
 
 /* Add a history item to the end of the list.
    List* list - the linked list
    char* str - the string to store
 */
-void add_history(List *list, char *str);
+void add_history(List *list, char *str){
+  Item *current = list -> root;
+  if(current == NULL){
+    Item *new = malloc(sizeof(Item));
+    new -> id = 0;
+    new -> str = str;
+    new -> next = NULL;
+    list ->root = new;
+  }
+  else{
+    while(current->next != NULL){
+      current = current->next;
+    }
+    Item *tempData = malloc(sizeof(Item));
+    tempData ->id = current->id+1;
+    tempData->str = str;
+    tempData->next = NULL;
+    current->next = tempData;
+  }
+}
+
 
 /* Retrieve the string stored in the node where Item->id == id.
    List* list - the linked list
    int id - the id of the Item to find */
-char *get_history(List *list, int id);
+char *get_history(List *list, int id){
+  Item * current = list->root;
+  while(current!= NULL){
+    if(current->id == id){
+      return current->str;
+    }
+    current=current->next;
+  }
+}
 
 /*Print the entire contents of the list. */
-void print_history(List *list);
+void print_history(List *list){
+  Item *current=list ->root;
+  while(current !=NULL){
+    printf("%s", current->str);
+    current =current->next;
+  }
+}
 
 /*Free the history list and the strings it references. */
-void free_history(List *list);
-
-#endif
+void free_history(List *list){
+   Item *current= list->root;
+   Item *temp;
+   while(current != NULL){
+     temp= current;
+     free(temp->str);
+     current = current -> next;
+     free(temp);
+   }
+   free(list);
+ }
